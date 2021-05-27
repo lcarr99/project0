3a73d0d57d8e2490acde34b64f4ba39e035b2e99
lcarr99 <carrdrfc99@gmail.com>
2021-05-27 17:22:46 +0100

    made the website more responsive

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@ $(document).ready(function() {
         $('.hiddenToStart').each(function(i) {
             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var width = $(window).width()
 
-            if (bottom_of_window > bottom_of_element) {
+            if (bottom_of_window > bottom_of_element && width >= 768) {
 
                 $(this).animate({ 'opacity': '1' }, 300);
 
